7066fdafcd92a67dbbdc8cb99a2e5c16e3ce2ed6
Josh Brown <josh@roadfiresoftware.com>
2013-09-10 22:08:39 -0400

    started video capture for Chapter 5

---
+++ BepBop/BepBop/BEPCaptureVideoViewController.h
@@ -0,0 +1,13 @@
+//
+//  BEPCaptureVideoViewController.h
+//  BepBop
+//
+//  Created by Josh Brown on 9/10/13.
+//  Copyright (c) 2013 Bleeding Edge Press. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BEPCaptureVideoViewController : UIViewController
+
+@end
