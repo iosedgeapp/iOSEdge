5576358700e3039b119b1458676e441513a98168
Josh Brown <josh@roadfiresoftware.com>
2013-09-11 21:48:39 -0400

    added maps; reorganized a bit

---
+++ BepBop/BepBop/BEPMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  BEPMapViewController.h
+//  BepBop
+//
+//  Created by Josh Brown on 9/11/13.
+//  Copyright (c) 2013 Bleeding Edge Press. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BEPMapViewController : UIViewController
+
+@end
---
+++ BepBop/BepBop/BEPMapViewController.m
@@ -0,0 +1,41 @@
+//
+//  BEPMapViewController.m
+//  BepBop
+//
+//  Created by Josh Brown on 9/11/13.
+//  Copyright (c) 2013 Bleeding Edge Press. All rights reserved.
+//
+
+#import "BEPMapViewController.h"
+#import <MapKit/MapKit.h>
+
+@interface BEPMapViewController ()
+
+@property IBOutlet MKMapView *mapView;
+
+@end
+
+@implementation BEPMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.mapView setUserTrackingMode:MKUserTrackingModeFollow animated:YES];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
