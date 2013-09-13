52a3d908218808a4a88155d4a42f2feaafd0b032
Engin Kurutepe <engin@kurutepe.com>
2013-09-13 12:35:07 +0200

    can now receive PDF files over AirDrop

--- BepBop/BepBop/BEPAppDelegate.m
+++ BepBop/BepBop/BEPAppDelegate.m
@@ -10,10 +10,15 @@
 #import "BEPMainViewController.h"
 #import "BEPNavigationController.h"
 
+#define BEPDefaultsStoredAirDropURLsKey @"BEPDefaultsStoredAirDropURLsKey"
+
 @implementation BEPAppDelegate
 
 - (BOOL) application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
 {
+    
+    [self handleSavedAirDropURLs];
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
@@ -27,11 +32,30 @@
     {
         navigationController.navigationBar.barTintColor = [UIColor lightGrayColor];
     }
-
+    
     self.window.rootViewController = navigationController;
 
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    if (sourceApplication == nil &&
+        [[url absoluteString] rangeOfString:@"Documents/Inbox"].location != NSNotFound) // Incoming AirDrop
+    {
+        NSLog(@"%@ sent from %@ with annotation %@", url, sourceApplication, [annotation description]);
+        if (application.protectedDataAvailable) {
+            [self moveToLocalDirectoryAirDropURL:url];
+        }
+        else {
+            [self saveAirDropURL:url];
+        }
+        return YES;
+    }
+
+    return NO;
 }
 
 - (void) applicationWillResignActive:(UIApplication*)application
@@ -60,5 +84,85 @@
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - AirDrop File Handling
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+- (void)saveAirDropURL:(NSURL *)url
+{
+    // we dont' have access to the file let's just remember the url for later handling
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSArray * currentStoredURLs = [defaults arrayForKey:BEPDefaultsStoredAirDropURLsKey];
+    
+    if (currentStoredURLs == nil) {
+        currentStoredURLs = @[];
+    }
+    
+    NSArray * updatedStoredURLs = [currentStoredURLs arrayByAddingObject:url];
+    
+    [defaults setObject:updatedStoredURLs forKey:BEPDefaultsStoredAirDropURLsKey];
+    
+    [defaults synchronize];
+}
+
+- (BOOL)moveToLocalDirectoryAirDropURL:(NSURL *)url
+{
+    NSFileManager * fm = [NSFileManager defaultManager];
+    
+    NSURL * localUrl = [[self airDropDocumentsDirectory] URLByAppendingPathComponent:[url lastPathComponent]];
+    NSError * error;
+    BOOL ret = [fm moveItemAtURL:url
+                           toURL:localUrl
+                           error:&error];
+    
+    if (!ret) {
+        NSLog(@"could not move file from %@ to %@: %@", url, localUrl, error);
+    }
+    
+    return ret;
+}
+
+- (NSURL*)airDropDocumentsDirectory
+{
+    NSFileManager * fm = [NSFileManager defaultManager];
+    NSURL * documentsDirectory = [[fm URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+    
+    NSURL * airDropDirectory = [documentsDirectory URLByAppendingPathComponent:@"AirDrop"];
+    
+    NSError * error;
+    if (![fm createDirectoryAtURL:airDropDirectory
+     withIntermediateDirectories:YES
+                      attributes:nil
+                           error:&error]) {
+        NSLog(@"couldn not create %@: %@", airDropDirectory, error);
+        return nil;
+    }
+    
+    return airDropDirectory;
+    
+}
+
+- (void)handleSavedAirDropURLs {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSArray * currentStoredURLs = [defaults arrayForKey:BEPDefaultsStoredAirDropURLsKey];
+    
+    if ([currentStoredURLs count]) {
+        NSMutableArray * unsavedURLs = [NSMutableArray arrayWithCapacity:[currentStoredURLs count]];
+        for (NSURL * url in currentStoredURLs) {
+            if (![self moveToLocalDirectoryAirDropURL:url]) {
+                [unsavedURLs addObject:url];
+            }
+        }
+        
+        [defaults setObject:[NSArray arrayWithArray:unsavedURLs]
+                     forKey:BEPDefaultsStoredAirDropURLsKey];
+        
+        [defaults synchronize];
+    }
+}
+
+
 
 @end
