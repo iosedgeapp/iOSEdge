6bac4e9b04e987505c6c3409f84bc8b55cd1df75
Michael Ang <mangtronix@gmail.com>
2013-09-14 22:48:30 +0200

    Gravity example with collisions

--- BepBop/BepBop/BEPDynamicsHeavyViewController.h
+++ BepBop/BepBop/BEPDynamicsHeavyViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface BEPDynamicsHeavyViewController : UIViewController
+@interface BEPDynamicsHeavyViewController : UIViewController <UICollisionBehaviorDelegate>
 
 @end
--- BepBop/BepBop/BEPDynamicsHeavyViewController.m
+++ BepBop/BepBop/BEPDynamicsHeavyViewController.m
@@ -30,6 +30,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+
+    // We'll set the color red later when there is an active collision
+    self.heavyLabel.textColor = [UIColor greenColor];
     
     self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
     
@@ -39,6 +42,7 @@
     // Without any collision behavior, the heavy label would fall right off the screen
     UICollisionBehavior *collideWithBounds = [[UICollisionBehavior alloc] initWithItems:@[self.heavyLabel]];
     collideWithBounds.translatesReferenceBoundsIntoBoundary = YES; // Simple way to make bounds of reference view into a collision boundary
+    collideWithBounds.collisionDelegate = self; // So we get callbacks for collisions
     [self.animator addBehavior:collideWithBounds];
 
 }
@@ -47,6 +51,21 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark UICollisionBehaviorDelegate methods
+- (void)collisionBehavior:(UICollisionBehavior *)behavior beganContactForItem:(id<UIDynamicItem>)item withBoundaryIdentifier:(id<NSCopying>)identifier atPoint:(CGPoint)p
+{
+    if (item == self.heavyLabel) {
+        self.heavyLabel.textColor = [UIColor redColor];
+    }
+}
+
+- (void)collisionBehavior:(UICollisionBehavior *)behavior endedContactForItem:(id<UIDynamicItem>)item withBoundaryIdentifier:(id<NSCopying>)identifier
+{
+    if (item == self.heavyLabel) {
+        self.heavyLabel.textColor = [UIColor greenColor];
+    }
 }
 
 @end
