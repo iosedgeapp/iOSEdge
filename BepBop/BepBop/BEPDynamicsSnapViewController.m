cec7d6c3ff8aed305a975e1d38aa73d57326034b
Michael Ang <mangtronix@gmail.com>
2013-09-15 01:08:48 +0200

    Add damping in snap example

--- BepBop/BepBop/BEPDynamicsSnapViewController.m
+++ BepBop/BepBop/BEPDynamicsSnapViewController.m
@@ -14,6 +14,7 @@
 @property (nonatomic) UISnapBehavior *snapBehavior;
 
 @property (nonatomic,weak) IBOutlet UIView *snapView;
+@property (assign) CGFloat damping;
 
 @end
 
@@ -33,6 +34,8 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     
+    self.damping = 0.5; // Default damping for snapiness
+    
     UIDynamicAnimator *animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
     self.animator = animator;
 }
@@ -43,6 +46,7 @@
     
     // Create a new snap behavior
     UISnapBehavior* snapBehavior = [[UISnapBehavior alloc] initWithItem:self.snapView snapToPoint:point];
+    snapBehavior.damping = self.damping;
 
     // If there was a previous snap behavior, remove it
     if (self.snapBehavior) {
@@ -52,6 +56,12 @@
     // Add the new behavior
     [self.animator addBehavior:snapBehavior];
     self.snapBehavior = snapBehavior;
+}
+
+- (IBAction)dampingChanged:(id)sender
+{
+    UISlider *dampingSlider = sender;
+    self.damping = dampingSlider.value;
 }
 
 @end
