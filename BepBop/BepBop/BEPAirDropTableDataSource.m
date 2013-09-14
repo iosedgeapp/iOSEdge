03f05c964e63e6b844aab90ecda0d1abd7c8913a
Hiedi Utley <hutley@zappos.com>
2013-09-13 19:44:02 -0700

    fix warnings and get tests running

--- BepBop/BepBop/BEPAirDropTableDataSource.m
+++ BepBop/BepBop/BEPAirDropTableDataSource.m
@@ -30,12 +30,12 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    
+    return 0;
 }
 
 - (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    
+    return nil;
 }
 
 @end
