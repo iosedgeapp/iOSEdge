875f771858b016125452de790e2a7d4f4da525ff
Michael Ang <mangtronix@gmail.com>
2013-09-15 00:18:42 +0200

    Attachment dragging example

---
+++ BepBop/BepBop/BEPDynamicsAttachmentViewController.h
@@ -0,0 +1,13 @@
+//
+//  BEPDynamicsAttachmentViewController.h
+//  BepBop
+//
+//  Created by mangtronix on 9/14/13.
+//  Copyright (c) 2013 Bleeding Edge Press. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BEPDynamicsAttachmentViewController : UIViewController
+
+@end
