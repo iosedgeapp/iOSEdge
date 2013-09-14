7f5cafeec8b5b524674fb9cb091de4b755bbb72f
Doron Katz <doron@doronkatz.com>
2013-09-14 13:51:16 +1000

    General update to chapter 9 stuff. Buttons not working yet, just moved some things around and set some coordinates

--- BepBop/BepBop/BEPMapViewController.h
+++ BepBop/BepBop/BEPMapViewController.h
@@ -2,12 +2,13 @@
 //  BEPMapViewController.h
 //  BepBop
 //
-//  Created by Josh Brown on 9/11/13.
+//  Created by Doron Katz on 9/11/13.
 //  Copyright (c) 2013 Bleeding Edge Press. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
 @interface BEPMapViewController : UIViewController
-
+- (IBAction)changeMapType:(UISegmentedControl *)control;
 @end
--- BepBop/BepBop/BEPMapViewController.m
+++ BepBop/BepBop/BEPMapViewController.m
@@ -2,16 +2,27 @@
 //  BEPMapViewController.m
 //  BepBop
 //
-//  Created by Josh Brown on 9/11/13.
+//  Created by Doron Katz on 9/11/13.
 //  Copyright (c) 2013 Bleeding Edge Press. All rights reserved.
 //
 
 #import "BEPMapViewController.h"
 #import <MapKit/MapKit.h>
 
+
+#define kLAT -33.85822
+#define kLONG 151.21493
+
+#define kLATBondi -33.8910
+#define kLONGBondi 151.2777
+
+#define kLATLunaPark -33.8482
+#define kLONGLunaPark 151.2100
+
 @interface BEPMapViewController ()
 
 @property IBOutlet MKMapView *mapView;
+@property (nonatomic, assign) MKCoordinateRegion boundingRegion;
 
 @end
 
@@ -26,25 +37,94 @@
     return self;
 }
 
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self.mapView setUserTrackingMode:MKUserTrackingModeFollow animated:YES];
-    [self.mapView setCenterCoordinate:self.mapView.userLocation.coordinate animated:NO];
-
-    // DEVTODO: put a 3D button on the map?
     
-    double latitudeOffset = 0.001; // TODO: play with this number
-    CLLocationDegrees latitude = self.mapView.centerCoordinate.latitude - latitudeOffset;
-    CLLocationCoordinate2D eyeCoordinate = CLLocationCoordinate2DMake(latitude, self.mapView.centerCoordinate.longitude);
-    MKMapCamera *camera = [MKMapCamera cameraLookingAtCenterCoordinate:self.mapView.centerCoordinate fromEyeCoordinate:eyeCoordinate eyeAltitude:1];
-    [self.mapView setCamera:camera animated:NO]; // TODO: animate if desired
+    
+    CLLocationCoordinate2D  sydneyOperaHouseCoordinate = CLLocationCoordinate2DMake(kLAT, kLONG);
+    CLLocationCoordinate2D  bondiBeachCoordiante = CLLocationCoordinate2DMake(kLATBondi, kLONGBondi);
+    CLLocationCoordinate2D  lunaParkCoordinate = CLLocationCoordinate2DMake(kLATLunaPark, kLONGLunaPark);
+    
+    MKCoordinateRegion sydneyOperaHouseRegion = MKCoordinateRegionMakeWithDistance(sydneyOperaHouseCoordinate, 100, 100);
+    MKCoordinateRegion bondiBeachRegion = MKCoordinateRegionMakeWithDistance(bondiBeachCoordiante, 100, 100);
+    MKCoordinateRegion lunaParkRegion = MKCoordinateRegionMakeWithDistance(lunaParkCoordinate, 100, 100);
+    
+    self.mapView.region = sydneyOperaHouseRegion;
+    //Annotations    
+    MKPointAnnotation *pointOperaHouse = [[MKPointAnnotation alloc] init];
+    pointOperaHouse.coordinate = CLLocationCoordinate2DMake(kLAT, kLONG);
+    pointOperaHouse.title = @"Sydney Opera House";
+    
+    MKPointAnnotation *pointBondiBeach = [[MKPointAnnotation alloc] init];
+    pointBondiBeach.coordinate = CLLocationCoordinate2DMake(kLATBondi, kLONGBondi);
+    pointBondiBeach.title = @"Bondi Beach";
+    [self.mapView addAnnotations:@[pointOperaHouse, pointBondiBeach]];
+    [self.mapView selectAnnotation:pointOperaHouse animated:YES];
+    
+    //[self.mapView setUserTrackingMode:MKUserTrackingModeFollow animated:YES];
+    //[self.mapView setCenterCoordinate:sydneyOperaHouseCoordinate animated:NO];
+    
+    
+    //Map Camera
+    
+    //Centre Coordinate - Point on ground
+    
+    //Altitude - Height above map
+    
+    //Heading - Direction camera faces
+    
+    //Pitch - Angle camera tilts
+    
+    
+    MKMapCamera *camera = [MKMapCamera cameraLookingAtCenterCoordinate:sydneyOperaHouseRegion.center  fromEyeCoordinate:lunaParkRegion.center eyeAltitude:900];
+    camera.pitch = 60;
+    //camera.altitude = 1500;
+    [self.mapView setCamera:camera animated:NO];
+
+    
 }
 
+
+
+#pragma mark Storing and Restoring State
+
+
+-(void)encodeRestorableStateWithCoder:(NSCoder *)coder{
+    MKMapCamera* camera = [self.mapView camera];
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *docDir = [paths objectAtIndex: 0];
+    NSString* docFile = [docDir stringByAppendingPathComponent: @"BepBopMap.plist"];
+    
+    [NSKeyedArchiver archiveRootObject:camera toFile:docFile];
+  
+    [super encodeRestorableStateWithCoder:coder];
+}
+
+
+    
+- (void)decodeRestorableStateWithCoder:(NSCoder *)coder{
+        
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *docDir = [paths objectAtIndex: 0];
+        NSString* docFile = [docDir stringByAppendingPathComponent: @"BepBopMap.plist"];
+        
+        MKMapCamera* camera = [NSKeyedUnarchiver unarchiveObjectWithFile: docFile];
+        [self.mapView setCamera:camera];
+        
+        [super decodeRestorableStateWithCoder:coder];
+    }
+    
+    
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+        [super didReceiveMemoryWarning];
+        // Dispose of any resources that can be recreated.
 }
 
 @end
