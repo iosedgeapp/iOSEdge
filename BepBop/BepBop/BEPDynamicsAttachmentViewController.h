0161e4d5c09354c98f4cd754e7fa6a9fad38cc13
Michael Ang <mangtronix@gmail.com>
2013-09-17 20:56:29 +0200

    Everything but the middle name

--- BepBop/BepBop/BEPDynamicsAttachmentViewController.h
+++ BepBop/BepBop/BEPDynamicsAttachmentViewController.h
@@ -2,7 +2,7 @@
 //  BEPDynamicsAttachmentViewController.h
 //  BepBop
 //
-//  Created by https://github.com/mangtronix on 9/14/13.
+//  Created by Michael Ang <https://github.com/mangtronix> on 9/14/13.
 //  Copyright (c) 2013 Bleeding Edge Press. All rights reserved.
 //
 
