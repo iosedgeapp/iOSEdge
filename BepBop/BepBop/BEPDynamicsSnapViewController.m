23abadebe4ec35299e05a426f6ceb1f03a10cf51
Michael Ang <mangtronix@gmail.com>
2013-09-17 19:11:15 +0200

    Simplify, the more didactic to be.

--- BepBop/BepBop/BEPDynamicsSnapViewController.m
+++ BepBop/BepBop/BEPDynamicsSnapViewController.m
@@ -13,8 +13,11 @@
 @property (nonatomic) UIDynamicAnimator* animator;
 @property (nonatomic) UISnapBehavior* snapBehavior;
 
+// The view that snaps to the touch point
 @property (nonatomic, weak) IBOutlet UIView* snappyView;
-@property (assign) CGFloat damping;
+
+// Controls the springiness of the snap
+@property (nonatomic, weak) IBOutlet UISlider* dampingSlider;
 
 @end
 
@@ -35,20 +38,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 
-    self.damping = 0.5; // Default damping for snapiness
-
+    // Set up the animator using our view as the reference view / coordinate system
     UIDynamicAnimator* animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
     self.animator = animator;
-}
-
-// We provide a damping slider to control how springy the animation is.
-// When the user updates the slider, store the value so we can use it
-// when we start the spring animation.
-- (IBAction) dampingChanged:(id)sender
-{
-    UISlider* dampingSlider = sender;
-    
-    self.damping = dampingSlider.value;
 }
 
 - (IBAction) handleTapGesture:(UITapGestureRecognizer*)tapGesture
@@ -58,7 +50,9 @@
     // Create a new snap behavior
     UISnapBehavior* snapBehavior = [[UISnapBehavior alloc] initWithItem:self.snappyView snapToPoint:point];
 
-    snapBehavior.damping = self.damping;
+    // The damping controls how springy the snap animation is.  You
+    // can play with different damping values by changing the slider.
+    snapBehavior.damping = self.dampingSlider.value;
 
     // If there was a previous snap behavior, remove it
     if (self.snapBehavior)
