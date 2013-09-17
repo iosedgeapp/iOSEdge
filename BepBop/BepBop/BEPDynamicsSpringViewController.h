c9ef860854d126016035c53617f7c956c03ea730
Michael Ang <mangtronix@gmail.com>
2013-09-17 20:54:18 +0200

    Linkify name, fo' a little mo' fame.

--- BepBop/BepBop/BEPDynamicsSpringViewController.h
+++ BepBop/BepBop/BEPDynamicsSpringViewController.h
@@ -2,7 +2,7 @@
 //  BEPDynamicsSpringViewController.h
 //  BepBop
 //
-//  Created by mangtronix on 9/15/13.
+//  Created by https://github.com/mangtronix on 9/15/13.
 //  Copyright (c) 2013 Bleeding Edge Press. All rights reserved.
 //
 
